77499c2fdd062819ebf9d73245fbc5cfcba6e04a
Denis77443 <denis_pj77@mail.ru>
2018-08-02 12:37:25 +0300

    remove const in method register

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -30,7 +30,7 @@ export default {
   },
   methods: {
     async register () {
-      const response = await AuthenticationService.register({
+      await AuthenticationService.register({
         email: this.email,
         password: this.password
       })
